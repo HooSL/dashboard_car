7c664b021b2f574ae8e804ded3a3f822cf3fad7e
HooSL <leegntmd@naver.com>
2021-12-15 17:36:22 +0900

    1215

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import os
 import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from PIL import Image
 from eda_app import run_eda_app
+from ml_app import run_ml_app
 
+
+img = Image.open('data/car.jpg')
+st.set_page_config(page_title='자동차 가격 예측',page_icon=img,layout='wide',initial_sidebar_state='collapsed')
 def main():
     
     #제목
@@ -19,39 +23,14 @@ def main():
 
     #사이드바 설정
     if choice == 'home':
-        st.write('이 앱은 고객데이터와 자동차 구매에 대한 내용입니다. 고객의 정보를 입력하면, 얼마정도의 차량을 구매할 수 있는지 예측해줍니다.')
+        st.write('이 앱은 고객데이터와 자동차 구매에 대한 내용입니다.')
+        st.write('고객의 정보를 입력하면, 얼마정도의 차량을 구매할 수 있는지 예측해드립니다.')
         st.write('왼쪽의 사이드바에서 선택하세요.')
     #새로운 py파일에 함수를 만들어주고 온다
     elif choice =='EDA':
         run_eda_app()
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    elif choice =='ML':
+        run_ml_app()
 
 
 if __name__ == '__main__':
